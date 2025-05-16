9cbc0c80b7b10dd4fb89381d2e8bfb4847c1f92a
Pedro Henrique Freire de Oliveira <pedrobolina123@gmail.com>
2025-05-16 17:20:37 -0300

    feat: adiciona esqueleto das funções de alterar, excluir e status

---
+++ excluir.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <struct.c>
+#include <string.h>
+
+int excluir(){
+    
+}
--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
+#include <struct.c>
+#include <inserir.c>
+#include <alterar.c>
+#include <status.c>
+#include <excluir.c>
 
 int main(){
-
 }
